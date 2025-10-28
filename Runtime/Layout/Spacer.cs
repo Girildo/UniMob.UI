e26216e466998acfce4da7626ee917b5c067eceb
Giovanni Moscato <giovi94@gmail.com>
2025-10-28 17:41:28 +0100

    feat: implement Spacer

---
+++ Runtime/Layout/Spacer.cs
@@ -0,0 +1,27 @@
+namespace UniMob.UI.Layout
+{
+    /// <summary>
+    /// Represents a flexible spacer widget that can occupy space proportionally within <see cref="Row"/> and <see cref="Column"/> layouts.
+    /// </summary>
+    /// <remarks>The <see cref="Flex"/> property determines the proportional amount of space this spacer
+    /// occupies relative to other widgets in the same layout.</remarks>
+    public class Spacer : StatefulWidget
+    {
+        public int Flex { get; set; } = 1;
+        public override State CreateState() => new SpacerState();
+
+
+    }
+    public class SpacerState : HocState<Spacer>
+    {
+        public override Widget Build(BuildContext context)
+        {
+            return new Expanded()
+            {
+                Child = SizedBox.Shrink(),
+                Flex = Widget.Flex
+            };
+        }
+    }
+
+}
